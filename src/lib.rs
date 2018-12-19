975c8913c940f4817ac7b84887e883665b06c11e
Riey <creeper844@gmail.com>
2018-12-19 17:32:24 +0900

    Update to v0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use qni_connector_ws_rs::prelude::core::*;
+use qni_connector_ws_rs::prelude::core::c_api::ProgramEntryCtxArg;
 use std::slice;
 use std::str;
 
@@ -11,13 +11,13 @@ pub extern "C" fn qni_init_program() {
 
 #[no_mangle]
 pub unsafe extern "C" fn qni_start_program(
-    hub: SharedHubPtr,
+    ctx: ProgramEntryCtxArg,
     addr: *const u8,
     addr_len: usize,
 ) -> i32 {
     let addr = str::from_utf8_unchecked(slice::from_raw_parts(addr, addr_len));
 
-    match qni_connector_ws_rs::start_connector((*hub).clone(), addr.to_string()) {
+    match qni_connector_ws_rs::start_connector(&*ctx, addr) {
         Ok(_) => 0,
         Err(err) => {
             error!("ws err: {}", err);
